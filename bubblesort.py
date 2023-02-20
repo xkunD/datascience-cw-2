ed08487b6c364ba45b261e4a41edf37c3ead4de3
xkunD <103330637+xkunD@users.noreply.github.com>
2023-02-20 19:05:59 +0000

    update hybridsort

--- bubblesort.py
+++ bubblesort.py
@@ -18,23 +18,17 @@ class processArray:
     def hybridSort(self):
         data = self.numbers
         for i in range(len(data)-1):            #will get it sorted if n-1 items are sorted
-            swapped = False                    # will stop when no swap can be found by bubble
-            minindex = i
-            for j in range(i, len(data)-i-1):   #start from i and check i with i+1
-                if data[j+1] < data[minindex]: 
-                    minindex = j                #keep tracking min index
-                # print("compare between", data[j], data[j+1],", and data[min] now is", data[minindex])
+            swapped = False
+            for j in range(len(data)-i-1):
+                print("compare between", data[j], data[j+1])
                 if data[j] > data[j+1]: 
                     swapped = True
                     data[j], data[j+1] = data[j+1], data[j]
-            # print("min value = ", data[minindex])
-            if not swapped:                 # if need to change to above?
+            if not swapped:
                 break
-            if minindex != i:
-                data[i], data[minindex] = data[minindex], data[i]   # selection sort
-            
+
 def main():
-    arraysort = processArray(10)
+    arraysort = processArray(3)
     print(arraysort)
     arraysort.hybridSort()
     print(arraysort)
