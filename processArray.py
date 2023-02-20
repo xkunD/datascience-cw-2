606e8c42f79fc09a03c3da3a6d64ff5a551de224
xkunD <103330637+xkunD@users.noreply.github.com>
2023-02-20 18:16:45 +0000

    bubble sort implemented

--- processArray.py
+++ processArray.py
@@ -1,37 +1,38 @@
 import numpy as np
+
 class processArray:
 
     def __init__(self, size):
         self.size = 2
         self.setSize(size)
-        self.numbers=np.random.random([self.size])*100
-        self.numbers=self.numbers.round(0).astype(int)      #added by myself
+        self.numbers = np.random.random([self.size])*100
+        self.numbers = self.numbers.round(0).astype(int)      #added by myself
 
     def __str__(self):
         return "Array of "+str(self.size)+" numbers"+"\n"+str(self.numbers)
     
     def setSize(self,size):
-        if(size>=2):
+        if(size >= 2):
             self.size=size
 
     def hybridSort(self):
-        swapped = False
-        for i in range(self.size, 0, -1):
-            for j in range(self.size - i):
-                if(self.numbers[i] > self.numbers[i+1]):
-                    self.numbers[i], self.numbers[i+1] = self.numbers[i+1], self.numbers[i]
+        data = self.numbers
+        for i in range(len(data)-1):            #will get it sorted if n-1 items are sorted
+            swapped = False
+            for j in range(len(data)-i-1):
+                print("compare between", data[j], data[j+1])
+                if data[j] > data[j+1]: 
                     swapped = True
-            if(swapped == False):
-                return
-               
-            
+                    data[j], data[j+1] = data[j+1], data[j]
+            if not swapped:
+                break
 
 def main():
-    arraysort = processArray(20)
+    arraysort = processArray(3)
+    print(arraysort)
+    arraysort.hybridSort()
     print(arraysort)
 
 
 if __name__ == "__main__":
     main() 
-
-
