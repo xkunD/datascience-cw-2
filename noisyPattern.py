7b178e443fab8d7734d158b0f55b78855dea8b03
xkunD <103330637+xkunD@users.noreply.github.com>
2023-03-01 00:59:55 +0000

    update removeNoise

--- noisyPattern.py
+++ noisyPattern.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+
 class noisyPattern:
     def __init__(self): 
         self.image=np.full([80,80],255) 
@@ -23,8 +24,33 @@ class noisyPattern:
                     self.image[i,j]=255
         plt.imsave('noisy_pattern.png', self.image, cmap=plt.cm.gray)
 
+    def __getMajority(self, i, j):
+        pixels = self.image[max(i-1, 0):min(i+2,self.image.shape[0]), max(j-1, 0):min(j+2,self.image.shape[1])]
+
+        pixels_flat = pixels.reshape(-1)
+
+        black_count = np.count_nonzero(pixels_flat == 0)
+        white_count = np.count_nonzero(pixels_flat == 255) - 1
+
+        if black_count > white_count:
+            return 0
+        elif black_count < white_count:
+            return 255
+        else:
+            return self.image[i,j]
+    
+
     def removeNoise(self):
-        pass
+        change = True
+        while change == True:
+            change = False
+            for i in range(self.image.shape[0]):
+                for j in range (self.image.shape[1]):
+                    if self.image[i, j] == 255:
+                        if self.image[i, j] != self.__getMajority(i, j):
+                            self.image[i, j] = self.__getMajority(i, j)
+                            change = True
+        plt.imsave('noise_removed.png', self.image, cmap=plt.cm.gray)
 
     def invert(self):
         pass
@@ -33,7 +59,7 @@ def main():
     noisyp = noisyPattern()
     noisyp.setFigure()
     noisyp.addNoise()
-    # noisyp.removeNoise()
+    noisyp.removeNoise()
     # noisyp.filter1()
     
 if __name__ == "__main__":
