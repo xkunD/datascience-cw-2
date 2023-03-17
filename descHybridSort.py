8f86558729d37c62973266c34d6d9e9f788f9922
xkunD <103330637+xkunD@users.noreply.github.com>
2023-03-17 12:15:51 +0000

    update decHybridSort.py

--- descHybridSort.py
+++ descHybridSort.py
@@ -3,47 +3,51 @@ import numpy as np
 class sortArray:
 
     def __init__(self,size):
-        self.size=2
+        self.size = 2
         self.setSize(size) 
-        self.numbers=np.random.random([self.size])*100 
-        self.numbers=self.numbers.round(0)
+        self.numbers = np.random.random([self.size])*100 
+        self.numbers = self.numbers.round(0)
 
     def __str__(self):
-        return "Array of "+str(self.size)+" numbers"+"\n"+str(self.numbers)
+        return "Array of " + str(self.size) + " numbers" + "\n" + str(self.numbers)
 
-    def setSize(self,size): 
-        if(size>=2):
-            self.size=size
+    def setSize(self, size): 
+        if(size >= 2):
+            self.size = size
 
     def descHybridSort(self):
         swapped = True   
-        i = 0                     
-        while swapped and i < int(len(self.numbers)/2):        
-            # outer loop only need to do n/2(ground) times (example case with 3 elems)
+        i = 0
+
+        while swapped and i < int(len(self.numbers)/2):         # outer loop only need n/2(ground) times
             swapped = False                    
             maxIndex = len(self.numbers) - i - 1
-            for j in range(i, len(self.numbers) - i - 1):   
-                # inner loop start from i and check j with j+1
-                if self.numbers[j] < self.numbers[j+1]:         # bubble sort
+
+            for j in range(i, len(self.numbers) - i - 1):       # inner loop start from i and stop -i
+                
+                if self.numbers[j] < self.numbers[j+1]:         # bubble sort check j with j+1
                     swapped = True
                     self.numbers[j], self.numbers[j+1] = self.numbers[j+1], self.numbers[j]
+                                    
                 if self.numbers[j] > self.numbers[maxIndex]:    # keep maxIndex
                     maxIndex = j 
-            # selection sort
-            self.numbers[i], self.numbers[maxIndex] = self.numbers[maxIndex], self.numbers[i]
+            
+            self.numbers[i], self.numbers[maxIndex] = self.numbers[maxIndex], self.numbers[i]   # selection sort
             i += 1
-        print("End of outer loop: ", self.numbers)
-
-    # debugging : issue with 2 elems fixed with limit on while loop
+        
+        print("End of outer loop:", self.numbers)
 
 
 def main():
-    print("Original array")
-    A=sortArray(8)
-    print(A)
+    print("Original array:")
+    A = sortArray(1)
+    print(A, "\n")
+    
     A.descHybridSort()
-    print("Sorted array")
+    
+    print("\nSorted array:")
     print(A)
+
 
 if __name__ == "__main__":
     main() 
